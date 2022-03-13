844a8a8c4ef9b63eb6d81326de4194d7d6d30211
liu-jia-jun <3142467441@qq.com>
2022-03-13 22:42:59 +0800

    update

--- leetcode/src/code/A.java
+++ leetcode/src/code/A.java
@@ -1,29 +1,58 @@
 package code;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 /**
  * @author 刘佳俊
  */
 public class A {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        int temp;
-        for(int i=0;i<nums.length;i++){
-            if(0<nums[i]&&nums[i]<=nums.length){
-                nums[nums[i]]+=nums.length;
-            }else{
-                temp = nums[i]-nums.length;
-                nums[nums[temp]]+=nums.length;
+    public static void main(String[] args) {
+        System.out.println(decodeString("100[leetcode]"));
+    }
+
+    public static String decodeString(String s) {
+        Stack<String> operatorStack = new Stack<>();
+        Stack<Integer> numberStack = new Stack<>();
+        char[] chars = s.toCharArray();
+        boolean flag = false;
+        String operTemp = "";
+        Integer numberTemp;
+        String str="";
+        for (int i = 0; i < chars.length; i++) {
+            if ('0' <= chars[i] && chars[i] <= '9') {
+                if (flag) {
+                    numberStack.push(numberStack.pop()*10 + (chars[i] - 48));
+                } else {
+                    numberStack.push(chars[i] - 48);
+                    flag=true;
+                }
+            } else if (chars[i] == ']') {
+                operTemp="";
+
+                while (!"[".equals(operatorStack.peek())) {
+                    operTemp = (operatorStack.peek() != null ? operatorStack.pop() : "") + operTemp;
+//                    operTemp = operatorStack.pop()+operTemp;
+                }
+                // 将左中括号弹出来
+                operatorStack.pop();
+                numberTemp = numberStack.pop();
+                str = "";
+                for (int j = 0; j < numberTemp; j++) {
+                    str += operTemp;
+                }
+                operatorStack.push(str);
+                flag=false;
+
+            } else {
+                operatorStack.add("" + chars[i]);
+                flag = false;
             }
         }
-        for(int i=0;i<nums.length;i++){
-            if(0<nums[i]&&nums[i]<=nums.length){
-                list.add(i);
-            }
+        operTemp="";
+        for (String s1 : operatorStack) {
+            operTemp+=s1;
         }
-        return list;
+
+        return operTemp;
     }
 }
