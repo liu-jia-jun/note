b589097b09fb6aef40f0a5de4ec4b69857919e9c
liu-jia-jun <3142467441@qq.com>
2022-04-06 08:46:42 +0800

    update

--- data_structure_algorithm/src/data_structure/tree/ArrBinaryTreeDemo.java
+++ data_structure_algorithm/src/data_structure/tree/ArrBinaryTreeDemo.java
@@ -4,4 +4,29 @@ package data_structure.tree;
  * @author 刘佳俊
  */
 public class ArrBinaryTreeDemo {
+
+    static int[] arr;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void preOrder(int index) {
+        //如果数组为空，或者 arr.length = 0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+        }
+        //输出当前这个元素
+        System.out.println(arr[index]);
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+    }
+
+
 }
