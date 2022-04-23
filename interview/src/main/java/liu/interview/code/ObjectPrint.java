5a9a63466b00b50b815671355d3c2fa9e924e3a5
liu-jia-jun <3142467441@qq.com>
2022-04-23 23:20:33 +0800

    update

--- interview/src/main/java/liu/interview/code/ObjectPrint.java
+++ interview/src/main/java/liu/interview/code/ObjectPrint.java
@@ -1,7 +1,13 @@
 package liu.interview.code;
 
+import org.openjdk.jol.info.ClassLayout;
+
 /**
  * @author 刘佳俊
  */
 public class ObjectPrint {
+    public static void main(String[] args) {
+        CommonTestBean commonTestBean = new CommonTestBean();
+        System.out.println(ClassLayout.parseInstance(commonTestBean).toPrintable());
+    }
 }
