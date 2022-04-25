dac6476ae5c1212851b0f4a15f1bb26cef47ecc8
liu-jia-jun <3142467441@qq.com>
2022-04-25 23:19:03 +0800

    update

--- data_structure_algorithm/src/data_structure/tree/HuffmanTree.java
+++ data_structure_algorithm/src/data_structure/tree/HuffmanTree.java
@@ -13,10 +13,7 @@ public class HuffmanTree {
     public static void main(String[] args) {
         int arr[] = {13, 7, 8, 3, 29, 6, 1};
         Node root = createHuffmanTree(arr);
-
         root.infixOrder();
-
-
     }
 
     public static Node createHuffmanTree(int[] arr) {
@@ -25,12 +22,9 @@ public class HuffmanTree {
         for (int i = 0; i < arr.length; i++) {
             list.add(new Node(arr[i]));
         }
-
-
         while (list.size() > 1) {
             // 将这些节点根据data值进行排序
             Collections.sort(list);
-
             Node node1 = list.remove(0);
             Node node2 = list.remove(0);
             Node root = new Node(node1.getData() + node2.getData());
@@ -38,10 +32,7 @@ public class HuffmanTree {
             root.setRight(node2);
             list.add(root);
         }
-
         return list.remove(0);
-
-
     }
 
 
