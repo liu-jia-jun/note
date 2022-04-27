50d5b66d3d5779227c464ea7a9d9bed87a622a26
liu-jia-jun <3142467441@qq.com>
2022-04-27 22:03:25 +0800

    update

--- data_structure_algorithm/src/data_structure/tree/AVLNode.java
+++ data_structure_algorithm/src/data_structure/tree/AVLNode.java
@@ -1,5 +1,110 @@
-package data_structure.tree;/**
+package data_structure.tree;
+
+/**
  * @author 刘佳俊
  */
 public class AVLNode {
+    private int data;
+    private AVLNode left;
+    private AVLNode right;
+
+
+    public int leftHeight(){
+        return left==null?0:right.height();
+    }
+    public int rightHeigiht(){
+        return right==null?0:right.height();
+    }
+    public int height(){
+        return Math.max(left==null?0:left.height(), right==null?0: right.height())+1;
+    }
+
+
+    public AVLNode search(int data) {
+        if (this.data == data) {
+            return this;
+        } else {
+            if (this.data > data) {
+                return this.getLeft() != null ? this.getLeft().search(data) : null;
+            } else {
+                return this.getRight() != null ? this.getRight().search(data) : null;
+            }
+        }
+    }
+
+    public AVLNode searchParent(int data) {
+        if (this.getLeft() != null && this.getLeft().getData() == data || this.getRight() != null && this.getRight().getData() == data) {
+            return this;
+        } else {
+            if (this.getData() > data) {
+                return this.getLeft() != null ? this.getLeft().searchParent(data) : null;
+            } else {
+                return this.getRight() != null ? this.getRight().searchParent(data) : null;
+            }
+        }
+    }
+
+
+    public void ergodic(AVLNode node) {
+
+        if (node.getLeft() != null) {
+            ergodic(node.getLeft());
+        }
+        System.out.println(node);
+        if (node.getRight() != null) {
+            ergodic(node.getRight());
+        }
+    }
+
+    public void addNode(AVLNode node) {
+        if (this.data > node.data) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                this.left.addNode(node);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                this.right.addNode(node);
+            }
+        }
+    }
+
+
+    public AVLNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public AVLNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(AVLNode left) {
+        this.left = left;
+    }
+
+    public AVLNode getRight() {
+        return right;
+    }
+
+    public void setRight(AVLNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "AVLNode{" +
+                "data=" + data +
+                '}';
+    }
 }
--- data_structure_algorithm/src/data_structure/tree/BinarySortNode.java
+++ data_structure_algorithm/src/data_structure/tree/BinarySortNode.java
@@ -2,7 +2,7 @@ package data_structure.tree;
 
 /**
  * @author 刘佳俊
- *
+ * <p>
  * 二叉排序树的节点
  */
 public class BinarySortNode {
@@ -12,33 +12,57 @@ public class BinarySortNode {
     private BinarySortNode right;
 
 
-    public void ergodic(BinarySortNode node){
+    public BinarySortNode search(int data) {
+        if (this.data == data) {
+            return this;
+        } else {
+            if (this.data > data) {
+                return this.getLeft() != null ? this.getLeft().search(data) : null;
+            } else {
+                return this.getRight() != null ? this.getRight().search(data) : null;
+            }
+        }
+    }
 
-         if(node.getLeft()!=null){
-             ergodic(node.getLeft());
-         }
+    public BinarySortNode searchParent(int data) {
+        if (this.getLeft() != null && this.getLeft().getData() == data || this.getRight() != null && this.getRight().getData() == data) {
+            return this;
+        } else {
+            if (this.getData() > data) {
+                return this.getLeft() != null ? this.getLeft().searchParent(data) : null;
+            } else {
+                return this.getRight() != null ? this.getRight().searchParent(data) : null;
+            }
+        }
+    }
+
+
+    public void ergodic(BinarySortNode node) {
+
+        if (node.getLeft() != null) {
+            ergodic(node.getLeft());
+        }
         System.out.println(node);
-        if(node.getRight()!=null){
+        if (node.getRight() != null) {
             ergodic(node.getRight());
         }
     }
 
-    public void addNode(BinarySortNode node){
-        if(this.data>node.data){
-            if(this.left==null){
-                this.left=node;
-            }else{
+    public void addNode(BinarySortNode node) {
+        if (this.data > node.data) {
+            if (this.left == null) {
+                this.left = node;
+            } else {
                 this.left.addNode(node);
             }
-        }else{
-            if(this.right==null){
-                this.right=node;
-            }else{
+        } else {
+            if (this.right == null) {
+                this.right = node;
+            } else {
                 this.right.addNode(node);
             }
         }
     }
-
 
 
     public BinarySortNode(int data) {
