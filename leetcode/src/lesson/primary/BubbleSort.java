2e84e4097b22b61838fa2a887f870e3e57b133b0
liu-jia-jun <3142467441@qq.com>
2022-05-09 23:11:23 +0800

    update

--- leetcode/src/lesson/primary/BubbleSort.java
+++ leetcode/src/lesson/primary/BubbleSort.java
@@ -1,7 +1,36 @@
 package lesson.primary;
 
+import java.util.Arrays;
+
 /**
  * @author 刘佳俊
+ *
+ * 冒泡排序
  */
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1,8,5,1,6,7,3,4,6};
+
+        int[] ints = bubbleSort(arr);
+        System.out.println(Arrays.toString(ints));
+    }
+
+
+    public static int[] bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j] + arr[j + 1];
+                    arr[j + 1] = arr[j] - arr[j + 1];
+                    arr[j] = arr[j] - arr[j + 1];
+                }
+            }
+        }
+
+        return arr;
+
+    }
+
+
 }
--- leetcode/src/lesson/primary/InsertSort.java
+++ leetcode/src/lesson/primary/InsertSort.java
@@ -6,4 +6,14 @@ package lesson.primary;
  * 插入排序
  */
 public class InsertSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] insertSort(int[] arr){
+
+
+
+    }
+
 }
